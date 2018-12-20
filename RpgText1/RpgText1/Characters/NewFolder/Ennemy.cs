c7949674c0dfe85b8f7a549704a497dbbe07b2e6
Sumeet Singh <sumeet93@hotmail.fr>
2018-12-20 21:18:46 +0100

    #10
    new upload, #9 didn't worked

---
+++ RpgText1/RpgText1/Characters/NewFolder/Ennemy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace RpgText1
+{
+    public class Ennemy : ICharacter
+    {
+        public Enemy():base()
+        {
+
+        }
+
+        public string Name { get; set; }
+        public virtual Enums.CharacterTypes CharacterType => Enums.CharacterTypes.None;
+        public int Health { get; set; }
+        public int Money { get; set; }
+        public int Level { get; set; }
+
+    }
+}
