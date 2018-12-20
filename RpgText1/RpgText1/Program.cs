ab2cbd42268dd686aef9c506e5a5046c4e95e8d4
Sumeet Singh <sumeet93@hotmail.fr>
2018-12-20 17:52:18 +0100

    #6

--- RpgText1/RpgText1/Program.cs
+++ RpgText1/RpgText1/Program.cs
@@ -18,12 +18,33 @@ namespace RpgText1
 
         private static void BeginOfTheGame()
         {
-            Console.WriteLine("Quel joueur voulez vous etre ?");
-            Console.WriteLine("A) Guerrier");
-            Console.WriteLine("B) Mage");
-            Console.WriteLine("C) Paysan");
 
-            char TypeCharacter = Console.ReadKey().KeyChar;
+            string TypeChar;
+
+            do
+            {
+                //Console.Clear();
+                Console.WriteLine("Quel joueur voulez vous etre ?");
+                Console.WriteLine("A) Guerrier");
+                Console.WriteLine("B) Mage");
+                Console.WriteLine("C) Paysan");
+
+                TypeChar = Console.ReadLine().ToUpper();
+                Console.Clear();
+
+                Console.WriteLine(TypeChar);
+            } while (TypeChar == "A" || TypeChar == "B" || TypeChar == "C");
+
+            switch (TypeChar)
+            {
+                case "A":
+                    break;
+                case "B":
+                    break;
+                case "C":
+                    break;
+            }
+
         }
 
         static void IntroStory(Character character)
